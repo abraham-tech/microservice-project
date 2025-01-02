a21e79f2d57b80df63ff96afa46051c378ffcbdd
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-01 22:48:24 -0600

    Create organizationDto and configure model mapper

---
+++ organization-service/src/main/java/net/javaguides/organizationservice/dto/OrganizationDto.java
@@ -0,0 +1,25 @@
+package net.javaguides.organizationservice.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrganizationDto {
+    private Long id;
+    private String organizationName;
+    private String organizationDescription;
+    private String organizationCode;
+    private LocalDateTime createDate;
+}
